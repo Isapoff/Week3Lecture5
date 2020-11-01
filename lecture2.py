bfd159ca23cdcdd34526704a5f162c83987e021e
isapoff <iskender.saparov.is@gmail.com>
2020-11-02 00:10:23 +0600

    first commit

---
+++ lecture2.py
@@ -0,0 +1,175 @@
+# age = int(input('Enter your age: '))
+# if age > 18:
+#     print('200 som')
+# elif age >7:
+#     print('150 som')
+# else:
+#     print('0 som')
+# print('Ok')
+
+                                         
+                                         # tirnarnyi operator if v odnu stroku 
+# s = input('say something: ')
+# res = 'Hi, cat' if s == 'May' or s == 'Mur' else 'Who are you?'             
+# print(res)
+
+
+                                        #  while
+
+# while uslovie: 
+#     deistvie telo cicla
+
+# count = 5
+# while count <= 10:                             # s while mozhno sozdat' beskonechnyi cikl
+#     print(count)
+#     count += 1
+
+
+# names = ['adilet', 'kylych', 'aijana']
+# print(names[0]
+# print(names[1])
+# print(names[2])
+
+# while len(names) > 0:
+#     print(names.pop().title())
+
+# while True:
+#     name = input('Enter your name: ')
+#     print(f'Hello, {name}')
+#     q = input("for exit enter 'q': ")
+#     if q.lower() == 'q':
+#         break 
+
+# flag = True
+# while flag:
+#     name = input('Enter your name: ')
+#     print(f'Hello, {name}')
+#     flag = input("for exit enter 'Enter': ")
+    
+# for var in iterable:
+#     body 
+
+
+# names = ['adilet', 'kylych', 'aijana']
+
+# for x in names: 
+#     print(x.title())
+
+# some_string = 'python'
+# count = 1
+
+# for i in some_string:
+#     print(f'Eto {count} iteracia')
+#     print(i)
+#     count +=1
+
+# nums = [1, 2, -3, 0, -5, 0]
+
+# for num in nums:
+#     if num > 0:
+#         print(f'{num} - positive')
+#     elif num < 0:
+#         print(f'{num} - negative')
+#     else:
+#         print(num)
+
+# nums = [1, 2, -3, 0, -5, 0]
+
+# for num in nums:
+#     if num > 0:
+#         print(f'{num} - positive')
+#     elif num < 0:
+#         print(f'{num} - negative')
+#     else: 
+#         continue
+#     print(num)
+
+
+                          # break zavershaet ves' cikl
+                          # continue zavershaet iteraciu no prodolzhaet cikl
+
+
+# nums = [1, 2, -3, 0, -5, 0]
+# new_list = []
+# for item in nums:
+#     if item > 0:
+#         new_list.append(item)
+#     else:
+#         break
+#     print(item) 
+# print(new_list)
+                                        
+# range(start, end, step)
+
+
+# list_ = list(range(0, 11, 2))          #range eto functiya  nujen dlya togo chtoby sozdavat' diapozon dlya cikla for
+# print(list_)          
+                                         #range - diapazon
+# for x in range(1, 11):
+#     print(x)
+
+# some_string = 'python'
+# for x in some_string
+# for x in range(len(some_string)):          #[0,1,2,3,4,5]
+    # print(some_string[x])
+
+# some_string = ['b', 'o','n', 'o', 'n', 'o']
+# for x in range(len(some_string)):
+#     if some_string[x] == 'o':
+#         some_string[x] = 'a'
+    
+
+# print(''.join(some_string))                  #pereformatiroval iz spiska v stroku
+
+# name = 'Isken'
+# print(len(name))
+# range(1, len(name), 2)
+# range(7)
+# [1,3,5]
+
+# x = 3
+# new = name[:x]
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
